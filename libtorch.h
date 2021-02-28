b7d28dd6f549a04ea3389ff67bf8c0b50b73fd50
syys96 <3359652182@qq.com>
2021-02-28 19:05:35 +0800

    先去吃饭

---
+++ libtorch.h
@@ -0,0 +1,48 @@
+//
+// Created by syys on 2021/2/28.
+//
+
+#ifndef EXAMPLE_APP_LIBTORCH_H
+#define EXAMPLE_APP_LIBTORCH_H
+
+#include <torch/script.h>  // One-stop header.
+
+#include <future>
+#include <memory>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "nogo.h"
+
+class NeuralNetwork {
+public:
+    using return_type = std::vector<std::vector<double>>;
+
+    NeuralNetwork(std::string model_path, bool use_gpu, unsigned int batch_size);
+    ~NeuralNetwork();
+
+    std::future<return_type> commit(Nogo* nogo);  // commit task to queue
+    void set_batch_size(unsigned int batch_size) {    // set batch_size
+        this->batch_size = batch_size;
+    };
+
+private:
+    using task_type = std::pair<torch::Tensor, std::promise<return_type>>;
+
+    void infer();  // infer
+
+    std::unique_ptr<std::thread> loop;  // call infer in loop
+    bool running;                       // is running
+
+    std::queue<task_type> tasks;  // tasks queue
+    std::mutex lock;              // lock for tasks queue
+    std::condition_variable cv;   // condition variable for tasks queue
+
+    std::shared_ptr<torch::jit::script::Module> module;  // torch module
+    unsigned int batch_size;                             // batch size
+    bool use_gpu;                                        // use gpu
+};
+
+#endif //EXAMPLE_APP_LIBTORCH_H
